78e2cffe0bdcd5fcd55718013df9991a1c31ba33
Tashif Ahmad Khan <tashifkhan010@gmail.com>
2026-03-05 01:57:34 +0530

    add missing posthog provider file

---
+++ website/lib/posthog-provider.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import posthog from "posthog-js";
+import { PostHogProvider as PHProvider } from "posthog-js/react";
+import { useEffect, Suspense } from "react";
+import { usePathname, useSearchParams } from "next/navigation";
+
+export function PostHogProvider({ children }: { children: React.ReactNode }) {
+  useEffect(() => {
+    if (typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY, {
+        api_host: "/ph/ingest",
+        ui_host: "https://eu.posthog.com",
+        person_profiles: "identified_only",
+        capture_pageview: false, // Disable automatic pageview capture, as we capture manually
+      });
+    }
+  }, []);
+
+  return (
+    <PHProvider client={posthog}>
+      <Suspense fallback={null}>
+        <PostHogPageView />
+      </Suspense>
+      {children}
+    </PHProvider>
+  );
+}
+
+function PostHogPageView() {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    // Track pageviews
+    if (pathname && typeof window !== "undefined" && process.env.NEXT_PUBLIC_POSTHOG_KEY) {
+      let url = window.origin + pathname;
+      if (searchParams && searchParams.toString()) {
+        url = url + `?${searchParams.toString()}`;
+      }
+      posthog.capture("$pageview", {
+        $current_url: url,
+      });
+    }
+  }, [pathname, searchParams]);
+
+  return null;
+}
